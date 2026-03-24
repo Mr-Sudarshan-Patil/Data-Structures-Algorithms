25bdc46dc8d1298c724134dcbc1222a5ca880ca7
Mr-Sudarshan-Patil <mr.sudarsh@gmail.com>
2026-03-24 21:41:31 +0530

    Day 1 first commit

---
+++ Day1.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+
+//  Sum of Two numbers
+
+let a = 20;
+let b = 30;
+
+console.log("sum of a + b", a+b);
+
+//  Sum of Two numbers using Third variable
+
+let c = 30;
+let d = 40;
+
+let sum = c+d;
+
+console.log("sum of c + d :", sum);
+
+//  Sum of number plus String 
+
+console.log(10 + "10"); // 1010
+console.log("10" + 10); // 1010
+
+// if we use plus sign it will consider as concatination other sign conver them to number
+
+console.log(10 - "10"); // 0
+console.log("10" - 10); // 0
